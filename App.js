a827fd390f6a245205d583049911289b5def1d2c
leocassiosilva <leocassiosilva1234@gmail.com>
2021-07-01 21:25:59 -0300

    first commit

--- App.js
+++ App.js
@@ -1,21 +1,36 @@
 import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { Component } from 'react';
+import { StyleSheet, Text, View, FlatList } from 'react-native';
+import Pessoas from './src/Pessoa/Pessoas';
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+class App extends Component{
+  
+  constructor(props){
+    super(props);
+    this.state = {
+      lista: [
+        {id: '1', nome:'Matheus', idade:50, email:'matheus@matheus.com'},
+        {id: '2', nome:'Pedro', idade:21, email:'pedro@pedro.com'},
+        {id: '3', nome:'Aluisio', idade:35, email:'aluisio@aluisio.com'},
+        {id: '4', nome:'José', idade:28, email:'jose@jose.com'},
+        {id: '5', nome:'Maria', idade:45, email:'maria@maria.com'},
+      ]
+    };
+  }
+
+  render(){
+    return (
+      <View style={styles.container}>
+        <FlatList data={this.state.lista} keyExtractor={(item)=> item.id} renderItem={({item}) => <Pessoas data={item}/> } />
+      </View>
+    );
+  }
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
   },
 });
+
+export default App; 
